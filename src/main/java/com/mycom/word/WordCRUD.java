770fa5d5a24eaf33637e45b553dcfa99f51c354d
Che60 <junyoung833@gmail.com>
2023-09-16 22:27:04 +0900

    edit project

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -1,11 +1,13 @@
 package com.mycom.word;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
     ArrayList<Word> list;
     Scanner s;
+    final String fname = "Dictionary.txt";
     WordCRUD(Scanner s){
         list = new ArrayList<>();
         this.s = s;
@@ -17,13 +19,13 @@ public class WordCRUD implements ICRUD{
         int level = s.nextInt();
         String word = s.nextLine();
 
-        System.out.print("뜻 입력:");
+        System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();
 
         return new Word(0, level, word, meaning);
     }
 
-    public void addWord(){
+    public void addItem(){
         Word one = (Word)add();
         list.add(one);
         System.out.println("새 단어가 단어장에 추가되었습니다. ");
@@ -84,5 +86,59 @@ public class WordCRUD implements ICRUD{
         Word word = list.get(idlist.get(id-1));
         word.setMeaning(meaning);
         System.out.println("단어가 수정되었습니다. ");
+    }
+
+    public void deleteItem() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 검색 : ");
+        int id = s.nextInt();
+        s.nextLine();
+
+        System.out.print("=> 정말로 삭제하실래요? (Y/n) ");
+        String ans = s.next();
+        if(ans.equalsIgnoreCase("y")){
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다. ");
+        } else
+            System.out.println("취소되었습니다. ");
+    }
+
+    public void loadFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
+
+            while(true){
+                line = br.readLine();
+                if(line == null) break;
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("==>" + count + "개 로딩 완료!!!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveFile() {
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(fname));
+            for(Word one : list){
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장 완료 !!!");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
